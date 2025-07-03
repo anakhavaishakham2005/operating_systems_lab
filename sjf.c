1e07e9c4eb02ed6c81369d38abd194350509146e
Anakha S <144688034+anakhavaishakham2005@users.noreply.github.com>
2025-07-03 23:11:01 +0530

    Update sjf.c

--- sjf.c
+++ sjf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define N 20
 
-int pid[N], at[N], bt[N], ct[N], tat[N], wt[N];
+int pid[N], at[N], bt[N], ct[N], tat[N], wt[N], done[N];
 int n, gantt[N];
 
 void input() {
@@ -14,36 +14,38 @@ void input() {
         scanf("%d", &at[i]);
         printf("Burst time of P%d: ", pid[i]);
         scanf("%d", &bt[i]);
+        done[i] = 0;
     }
 }
 
-void sort_by_arrival() {
-    for(int i = 0; i < n-1; i++) {
-        for(int j = 0; j < n-1-i; j++) {
-            if(at[j] > at[j+1]) {
-                // Swap arrival time, burst time, process ID
-                int temp;
-                temp = at[j]; at[j] = at[j+1]; at[j+1] = temp;
-                temp = bt[j]; bt[j] = bt[j+1]; bt[j+1] = temp;
-                temp = pid[j]; pid[j] = pid[j+1]; pid[j+1] = temp;
-            }
+int find_shortest(int t) {
+    int idx = -1, min_bt = 1e9;
+    for(int i = 0; i < n; i++) {
+        if(at[i] <= t && !done[i] && bt[i] < min_bt) {
+            min_bt = bt[i];
+            idx = i;
         }
     }
+    return idx;
 }
 
-void fcfs() {
-    int curr_time = 0;
-    gantt[0] = curr_time;
+void sjf() {
+    int completed = 0, curr = 0, g = 0;
+    gantt[g++] = curr;
 
-    for(int i = 0; i < n; i++) {
-        if(curr_time < at[i])
-            curr_time = at[i]; // CPU idle
+    while(completed < n) {
+        int idx = find_shortest(curr);
+        if(idx == -1) {
+            curr++; // idle
+            continue;
+        }
 
-        curr_time += bt[i];
-        ct[i] = curr_time;
-        tat[i] = ct[i] - at[i];
-        wt[i] = tat[i] - bt[i];
-        gantt[i+1] = curr_time;
+        curr += bt[idx];
+        ct[idx] = curr;
+        tat[idx] = ct[idx] - at[idx];
+        wt[idx] = tat[idx] - bt[idx];
+        done[idx] = 1;
+        gantt[g++] = curr;
     }
 }
 
@@ -55,11 +57,17 @@ void print_table() {
 }
 
 void print_gantt() {
-    printf("\nGantt Chart:\n-------------------------------------------------\n");
-    for(int i = 0; i < n; i++) {
-        printf("|  P%d  ", pid[i]);
+    printf("\nGantt Chart:\n--------------------------------------------------\n");
+    int last = 0;
+    for(int i = 1; i < n+1; i++) {
+        for(int j = 0; j < n; j++) {
+            if(ct[j] == gantt[i]) {
+                printf("|  P%d  ", pid[j]);
+                break;
+            }
+        }
     }
-    printf("|\n-------------------------------------------------\n");
+    printf("|\n--------------------------------------------------\n");
     for(int i = 0; i <= n; i++) {
         printf("%d\t", gantt[i]);
     }
@@ -74,8 +82,7 @@ float avg(int arr[]) {
 
 int main() {
     input();
-    sort_by_arrival();
-    fcfs();
+    sjf();
     print_table();
     print_gantt();
     printf("\nAverage Waiting Time: %.2f", avg(wt));
